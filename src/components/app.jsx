2416e64dfb73e5240c39b148aa8a381b09b86eb8
Pranav Ajay <pranavajay2002@gmail.com>
2021-11-30 12:51:35 +0530

    Added dummy notes

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Note from "./notes";
+import notes from "../notes";
+
+const NoteGen = (note) => (
+    <Note 
+        key = {note.key}
+        title = {note.title}
+        content = {note.content}
+    />
+
+
+);
 
 function App(){
     return (
     <div>
     <Header/>
     
-        <Note/>
+        {notes.map(NoteGen)}
         <Footer/>
  
     </div>
