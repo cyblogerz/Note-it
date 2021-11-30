9438c4f52bafa8599dcc8a2266698333cadd7369
Pranav Ajay <pranavajay2002@gmail.com>
2021-11-30 14:03:53 +0530

    Changed name

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header(){
     return(
         <header>
-            <h1>Note !t</h1>
+            <h1><code>Note !t</code></h1>
         </header>
     );
 }
--- src/components/notes.jsx
+++ src/components/notes.jsx
@@ -6,8 +6,8 @@ import React from "react";
 function Note(props){
     return(
         <div className="note">
-        <h1>{props.title}</h1>
-        <p>{props.content}</p>
+        <h1><code>{props.title}</code></h1>
+        <p><code>{props.content}</code></p>
 
         </div>);
 }
